d68761c85518babb85bc7c015a275a92aacd0e49
VittaldasRKamath <vittaldaskamath25.xworkz@gmail.com>
2023-08-21 11:50:08 +0530

    See the fruits available

---
+++ Fruits.java
@@ -0,0 +1,16 @@
+class Fruits
+{
+import java.util.Arrays;
+
+public class StringArraySorting {
+    public static void main(String[] args { String[] arrayOfStrings = {"apple", "banana", "grape", "orange"};
+        
+        // Sort the array in ascending order
+        Arrays.sort(arrayOfStrings);
+        
+        // Print the sorted array
+        for (String str : arrayOfStrings)
+            System.out.println(str);
+        }
+    }
+}
