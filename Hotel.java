f4a258c7fe7bda2bf9fa6a8583466bae242952f7
VittaldasRKamath <vittaldaskamath25.xworkz@gmail.com>
2023-08-21 12:45:05 +0530

    Finf the Hotel

---
+++ Hotel.java
@@ -0,0 +1,19 @@
+class Hotel
+{
+	public static void main (String Hotel[])
+	{
+		//Datatypes variableName=value/data;
+	String name =  "Kamath Hotel";
+	String address = "Karwar";
+	String foodMenu = "Idli, Dosa, GheeRice, Paneer Tikka";
+	String ownerName = "Vittaldas R Kamath";
+	int noOfTable = 100;
+	double rating = 4.8;
+		System.out.println("The Name of the Hotel       :" +name);
+		System.out.println("The Address of the Hotel    :" +address);
+		System.out.println("The FoodMenu of the Hotel   :" +foodMenu);
+		System.out.println("The OwnerName of the Hotel  :" +ownerName);
+		System.out.println("The NoOfTable of the Hotel  :" +noOfTable);
+		System.out.println("The Rating of the Hotel     :" +rating);
+	}                                                    
+}	
