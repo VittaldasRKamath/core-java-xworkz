8b4ae67b22231dc96ec5219592a6322b0733429c
VittaldasRKamath <vittaldaskamath25.xworkz@gmail.com>
2023-08-21 22:00:43 +0530

    Day 3 Tasks

---
+++ Hospital.java
@@ -0,0 +1,17 @@
+class Hospital
+{
+	public static void main (String hospital[])
+	{
+	String name =  "Appolo Hospital";
+	String location = "Jayanagar";
+	String noOfPatients = "400";
+	String noOfBeds = "500";
+	String noOfElevators = "16";
+	
+		System.out.println("The Name of the Hospital         :" +name);
+		System.out.println("The Height of the Hospital       :" +location);
+		System.out.println("The Weight of the Hospital       :" +noOfPatients);
+		System.out.println("The Gender of the Hospital       :" +noOfBeds);
+		System.out.println("The Age of the Hospital          :" +noOfElevators);	
+	}                                                    
+}	
---
+++ Studio.java
@@ -0,0 +1,17 @@
+class Studio
+{
+	public static void main (String Studio[])
+	{
+		//Datatypes variableName=value/data;
+	String name =  "Adarsh Recording Studio";
+	String adress = "Rajajinagar";
+	String ownerName = "Vittaldas Kamath";
+	String gstNo = "29GGGG1314R9Z6";
+	String phoneNo = "8023329705";
+		System.out.println("The Name of the Studio     :" +name);
+		System.out.println("The Adress of the Studio   :" +adress);
+		System.out.println("The OwnerName of the Studio:" +ownerName);
+		System.out.println("The GSTNo of the Studio    :" +gstNo);
+		System.out.println("The PhoneNo of the Studio  :" +phoneNo);
+	}                                                    
+}	
