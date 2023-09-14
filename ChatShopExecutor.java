db9c9f9d13333050008129485b81a100b13ce326
VittaldasRKamath <vittaldaskamath25.xworkz@gmail.com>
2023-09-14 10:12:22 +0530

    CRUD Examples

---
+++ ChatShopExecutor.java
@@ -0,0 +1,17 @@
+class ChatShopExecutor{
+    public static void main(String args[]){
+	     Chatshop cs = new Chatshop();
+		 cs.addChats("Pani Puri");
+		 cs.addChats("Masala Puri");
+		 cs.addChats("Bhel Puri");
+		 cs.addChats("Dahi Puri");
+		 cs.addChats("Sukha Puri");
+		 cs.addChats("Gobi Manchoori");
+		 cs.addChats("Baby Corn Manchoori");
+		 cs.addChats("Mushroom Manchoori");
+		 cs.addChats("Paneer Manchoori");
+		 cs.addChats("Samosa");
+	
+	    cs.getDetails();
+	}
+}
---
+++ OttExecutor.java
@@ -0,0 +1,20 @@
+class OttExecutor{
+
+
+    public static void main (String ottp[]){
+     Ott ot = new Ott();
+	 ot.addMovies("Om");
+	 ot.addMovies("Bachchan");
+	 ot.addMovies("Max");
+	 ot.addMovies("Jailor");
+	 
+	 ot.getDetails();
+	}
+}
+
+
+/* ChatShop
+   Hotel
+   cricket
+   spotify
+   interiors*/
